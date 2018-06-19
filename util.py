7df78bc4c10b9b54056148b1ce792c2066f0694e
Naveen Raman <dsfan414@gmail.com>
2018-06-19 13:36:09 -0400

    Added function in util to read TSV files

--- util.py
+++ util.py
@@ -2,3 +2,12 @@ import numpy as np
 
 def makeRandom(m,n):
     return np.random.rand(m,n)
+
+def readTSV(name):
+	t = []
+	f = open(name).read().split("\n")[1:-1]
+	for i in range(len(f)):
+		t.append(list(map(int,f[i].split("\t")[1:])))
+	return np.array(t)
+
+print(readTSV("mutation-counts.tsv"))
